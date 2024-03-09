7985667b35ebf1f91b3d512d67bca226887f0405
maddd355 <158117673+maddd355@users.noreply.github.com>
2024-03-08 21:29:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function message() {
   let message = document.getElementById("message").value;
+  let name = document.getElementById("name").value;
   const webhook = https://discord.com/api/webhooks/1215890729117814814/F4mKSuHvvf__3oSvteF4-rmDoFwTlWiPea6F039-IiCc_m2E9KkzrPuCRBwmGv-nPc67;
-  const contents = `Message: ${message}`;
+  const contents = `Name: ${name}\nMessage: ${message}`;
   const request = new XMLHttpRequest();
   request.open("POST", webhook);
   request.setRequestHeader('Content-type', 'application/json');
